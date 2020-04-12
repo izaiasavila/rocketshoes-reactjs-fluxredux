6b4d6d0e74c154e532dfa031103676fdad48b356
Izaias Avila da Conceicao Filho <izaias.filho@selfitacademias.com.br>
2020-04-12 20:02:07 -0300

    Estilizando o home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <ProductList>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-olympikus-dynamic-masculino/06/D22-0697-006/D22-0697-006_zoom1.jpg?ts=1585887656&ims=326x"></img>
+        <strong>Tenis muito legal</strong>
+        <span>R$200</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+0
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
